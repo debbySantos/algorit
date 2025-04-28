5b444acb9a9de2ea92152d328304e3525dd957de
debbySantos <deborassantos880@gmail.com>
2025-04-28 20:00:42 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class App {
+    public static String obterNome(){
+        System.out.println("App.obterNome()");
+        Scanner input = new Scanner(System.in);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+  git config --global user.email "deborassantos880@gmail.com"
+  git config --global user.name "debbySantos"
