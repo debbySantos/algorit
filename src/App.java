c5e15f28783419264df29c92f7162c079ff34d8a
debbySantos <deborassantos880@gmail.com>
2025-04-28 20:35:18 -0300

    código Inicial

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
+
 public class App {
-    public static String obterNome(){
+    public static String obterNome() {
         System.out.println("App.obterNome()");
 
         Scanner input = new Scanner(System.in);
@@ -9,7 +10,7 @@ public class App {
         return nome;
     }
 
-    public static float nota(){
+    public static float nota() {
         System.out.println("App.nota()");
 
         Scanner input = new Scanner(System.in);
@@ -17,19 +18,56 @@ public class App {
         return nota;
     }
 
-    public static float calculoMedia(float nota1, float nota2, float nota3){
+    public static float calculoMedia(float nota1, float nota2, float nota3) {
         System.out.println("App.calculoMedia()");
 
-        float media = (nota1 + nota2 + nota3)/ 3;
+        float media = (nota1 + nota2 + nota3) / 3;
         return media;
     }
 
-    public static char conceito(float media){
+    public static char conceito(float media) {
         System.out.println("App.conceito()");
         char conceito;
-        if(media >= 9){
+        if (media >= 9 && media <= 10) {
             conceito = 'A';
+        } else if (media >= 8 && media < 9) {
+            conceito = 'B';
+        } else if (media >= 7 && media < 8) {
+            conceito = 'C';
+        } else if (media >= 6 && media < 7) {
+            conceito = 'D';
+        } else {
+            conceito = 'E';
         }
+
+        return conceito;
+    }
+
+    public static String status(char conceito) {
+        System.out.println("App.status()");
+
+        String status = "";
+        switch (conceito) {
+            case 'A':
+            case 'B':
+            case 'C':
+            case 'D':
+                status = "Aprovado!";
+                break;
+            case 'E':
+                status = "Reprovado!";
+
+        }
+        return status;
+    }
+
+    // imprimir != retornar
+
+    public static void imprimirDados(String nome, float media, char conceito, String status) {
+        System.out.println("App.imprimirDados()");
+
+        System.out.printf("Aluno: %s \n media: %.2f \n conceito: %c \n status: %s \n", nome, media, conceito, status);
+        
     }
 
     public static void main(String[] args) throws Exception {
