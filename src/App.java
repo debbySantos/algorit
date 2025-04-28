ac73132a87bb96f7676d88d9b5fd3f3f55384e70
debbySantos <deborassantos880@gmail.com>
2025-04-28 20:10:39 -0300

    f

--- src/App.java
+++ src/App.java
@@ -2,9 +2,23 @@ import java.util.Scanner;
 public class App {
     public static String obterNome(){
         System.out.println("App.obterNome()");
+
         Scanner input = new Scanner(System.in);
-        int tantoaz  = 1;
-        return "a";
+        System.out.println("Digite o nome do aluno");
+        String nome = input.nextLine();
+        return nome;
+    }
+
+    public static float nota(){
+        System.out.println("App.nota()");
+
+        Scanner input = new Scanner(System.in);
+        float nota = input.nextFloat();
+        return nota;
+    }
+
+    public static float calculoMedia(float nota1, float nota2, float nota3){
+        
     }
 
     public static void main(String[] args) throws Exception {
