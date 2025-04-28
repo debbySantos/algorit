77021663ac1a5455c31eb39d5937d7b95e7d173d
debbySantos <deborassantos880@gmail.com>
2025-04-28 20:07:02 -0300

    aaa

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ public class App {
     public static String obterNome(){
         System.out.println("App.obterNome()");
         Scanner input = new Scanner(System.in);
+        int tantoaz  = 1;
+        return "a";
     }
 
     public static void main(String[] args) throws Exception {
